f2944fc4ea6974333485f734cd6dd2bce0eaa256
Vinay Maheshwari <vinaymaheshwari35@gmail.com>
2022-12-20 16:20:25 +0530

    made changes

--- binarySearch.java
+++ binarySearch.java
@@ -1,19 +1,19 @@
-import java.lang.reflect.Array;
-import java.util.ArrayList;
-import java.util.Arrays;
+// import java.lang.reflect.Array;
+// import java.util.ArrayList;
+// import java.util.Arrays;
 
-public class binarySearch {
+// public class binarySearch {
 
-    public static void main(String[] args) {
-        int arr[] = { 10, 9, 8, 7, 6, 5, 4, 3, 2, 1, 0 };
-        binarySearch(arr, false);
-    }
+// public static void main(String[] args) {
+// int arr[] = { 10, 9, 8, 7, 6, 5, 4, 3, 2, 1, 0 };
+// binarySearch(arr, false);
+// }
 
-    public static void binarySearch(int arr[], boolean sorted) {
-        if (sorted) {
+// public static void binarySearch(int arr[], boolean sorted) {
+// if (sorted) {
 
-        } else {
-            arr = Arrays.sort(arr);
-        }
-    }
-}
+// } else {
+// arr = Arrays.sort(arr);
+// }
+// }
+// }
